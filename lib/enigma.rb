68eb3add72d93fcbdbeb40ed74046614bcd36b26
Robert Heath <robert.b.heathii@gmail.com>
2020-09-18 13:59:16 -0500

    Outline code for encrypt method in Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,4 +2,12 @@ require 'date'
 
 class Enigma
 
+  def encrypt(message, key, date)
+    encrypt_hash = {}
+    encrypt_hash[:encryption] = message.encrypted
+    encrypt_hash[:key] = key
+    encrypt_hash[:date] = date
+    encrypt_hash
+  end
+
 end
