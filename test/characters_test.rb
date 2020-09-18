080a81fbfae8882fd9102c39ddb7e510eabf6b54
Robert Heath <robert.b.heathii@gmail.com>
2020-09-18 16:02:07 -0500

    Test characters existence and set

---
+++ test/characters_test.rb
@@ -0,0 +1,17 @@
+require './test/test_helper'
+require './lib/characters'
+
+class CharactersTest < Minitest::Test
+
+  def test_it_exists_and_has_a_set
+    character = Characters.new
+
+    assert_instance_of Character, character
+    expected = ["a", "b", "c", "d", "e", "f",
+                "g", "h", "i", "j", "k", "l",
+                "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x",
+                "y", "z", " "]
+    assert_equal expected,character.set
+  end
+end
