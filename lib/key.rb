f07cb882b6f1ebd2478af5bde5420d96dee97a15
Robert Heath <robert.b.heathii@gmail.com>
2020-09-20 12:50:47 -0500

    Test and create random 4 digit number

--- lib/key.rb
+++ lib/key.rb
@@ -1,3 +1,8 @@
 class Key
+  attr_reader :random_4_digit_num
+
+  def initialize
+    @random_4_digit_num = rand(1000..9999)
+  end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/key'
+require 'mocha/minitest'
 
 class KeyTest < Minitest::Test
 
@@ -8,4 +9,14 @@ class KeyTest < Minitest::Test
 
     assert_instance_of Key, key1
   end
+
+  def test_it_creates_random_number
+    key1 = Key.new
+    key1.stubs(:random_4_digit_num).returns(9867)
+    assert_equal 9867, key1.random_4_digit_num
+  end
+
+  def test_it_adds_a_zero_to_random_number
+
+
 end
