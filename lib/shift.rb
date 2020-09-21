92e4af6682466b455d2ec0ca5b51260dbebfd6d4
Robert Heath <robert.b.heathii@gmail.com>
2020-09-20 22:08:36 -0500

    Test and code shifts a through d

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,7 +3,35 @@ require './lib/key'
 require './lib/offset'
 
 class Shift
+  attr_reader :keys, :offsets
 
-  def initialize
+  def initialize(date)
+    create_keys
+    create_offsets(date)
   end
+
+  def create_keys
+    @keys = Key.new
+  end
+
+  def create_offsets(date)
+    @offsets = Offset.new(date)
+  end
+
+  def shift_a
+    @keys.key_a.to_i + @offsets.offset_a.to_i
+  end
+
+  def shift_b
+    @keys.key_b.to_i + @offsets.offset_b.to_i
+  end
+
+  def shift_c
+    @keys.key_c.to_i + @offsets.offset_c.to_i
+  end
+
+  def shift_d
+    @keys.key_d.to_i + @offsets.offset_d.to_i
+  end
+
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,15 +5,20 @@ require 'mocha/minitest'
 class ShiftTest < Minitest::Test
 
   def test_it_exists
-    shift = Shift.new
+    shift = Shift.new("032986")
 
     assert_instance_of Shift, shift
   end
 
   def test_it_can_add_keys_and_offsets
-    shift = Shift.new
-    key = Key.new
-    offset = Offset.new
+    shift = Shift.new("032986")
+    shift.stubs(:shift_a).returns(15)
+    shift.stubs(:shift_b).returns(99)
+    shift.stubs(:shift_c).returns(95)
+    shift.stubs(:shift_d).returns(73)
+
+    assert_equal 2, shift.keys.key_a.length
+    assert_equal "6", shift.offsets.offset_a
 
     assert_equal 15, shift.shift_a
     assert_equal 99, shift.shift_b
