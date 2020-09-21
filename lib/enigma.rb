4e18afe72fb32332d09e6790696eff989f8b487c
Robert Heath <robert.b.heathii@gmail.com>
2020-09-21 12:29:24 -0500

    Test and create instance variables for enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,21 @@
 require 'date'
 
 class Enigma
+  attr_reader :message,
+              :key,
+              :date
 
   def encrypt(message, key, date)
     encrypt_hash = {}
-    encrypt_hash[:encryption] = message.encrypted
-    encrypt_hash[:key] = key
-    encrypt_hash[:date] = date
-    encrypt_hash
+    @message = message
+    @key = key
+    @date = date
+    # encrypt_hash[:encryption] = message.encrypt_that_puppy
+    # encrypt_hash[:key] = @key
+    # encrypt_hash[:date] = @date
+    # encrypt_hash
   end
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,7 +4,36 @@ require 'date'
 
 class EnigmaTest < Minitest::Test
 
-  def it_exists
+  def test_it_exists
     enigma = Enigma.new
   end
+
+  def test_enigma_has_attributes
+    enigma = Enigma.new
+    enigma.encrypt("hello world", "02715", "040895")
+
+    assert_equal "hello world", enigma.message
+    assert_equal "02715", enigma.key
+    assert_equal "040895", enigma.date
+  end
+
+  # def test_encrypt_that_puppy
+  #   enigma = Enigma.new
+  #   actual = enigma.encrypt_that_puppy("hello world", "02715", "040895")
+  #
+  #   assert_equal "keder ohulw", actual
+  # end
+
+  # def test_encrypt
+  #   enigma = Enigma.new
+  #   expected = ({ encryption: "keder ohulw",
+  #                        key: "02715",
+  #                       date: "040895"
+  #                       })
+  #   actual = enigma.encrypt("hello world", "02715", "040895")
+  #   assert_equal expected, actual
+  # end
+
+
+
 end
