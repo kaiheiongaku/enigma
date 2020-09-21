fefaf108ba0e978c36cda64312da47044db783bb
Robert Heath <robert.b.heathii@gmail.com>
2020-09-20 21:08:27 -0500

    Create test add_keys_and_offsets for Shift class

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -10,5 +10,14 @@ class ShiftTest < Minitest::Test
     assert_instance_of Shift, shift
   end
 
-  def test_it_has_keys_and_offsets
+  def test_it_can_add_keys_and_offsets
+    shift = Shift.new
+    key = Key.new
+    offset = Offset.new
+
+    assert_equal 15, shift.shift_a
+    assert_equal 99, shift.shift_b
+    assert_equal 95, shift.shift_c
+    assert_equal 73, shift.shift_d
+  end
 end
