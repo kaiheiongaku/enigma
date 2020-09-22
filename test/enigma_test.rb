37834cc6b260291945f9b0f6c3c354ab06b37508
Robert Heath <robert.b.heathii@gmail.com>
2020-09-21 22:16:12 -0500

    Commit to checkout to main

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,6 +6,8 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
   end
 
   def test_enigma_has_attributes
@@ -58,7 +60,4 @@ class EnigmaTest < Minitest::Test
     actual = enigma.encrypt("hello world", "02715", "040895")
     assert_equal expected, actual
   end
-
-
-
 end
